b297da8bc4aec95a6a43c762817f005f031834b5
DanielTolentino <danieltolentino@protonmail.com>
2021-10-28 18:28:33 -0300

    fix flatpak installs

--- setup/flatpaks.sh
+++ setup/flatpaks.sh
@@ -3,45 +3,24 @@ echo -e "\nInstalling Flatpaks\n"
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 PKGS=(
-'Adwaita'
 'Bottles'
 'Builder'
 'Byte'
-'Codecs'
-'default'
 'Evince'
 'Foliate'
-'Freedesktop'
-'GNOME'
-'GNU'
-'i386'
-'KDE'
 'Kdenlive'
 'LibreOffice'
-'Manual'
 'Markets'
-'Mesa'
-'openh264'
 'PDF'
-'QGnomePlatform'
-'QGnomePlatform-decoration'
-'QtSNI'
-'Rust'
 'Spotify'
 'Stremio'
-'SWH'
-'TAP-plugins'
 'Transmission'
 'ungoogled-chromium'
-'WhiteSur-dark'
-'WhiteSur-dark-solid'
-'WhiteSur-light'
-'WhiteSur-light-solid'
 'Zoom'
 )
 for PKG in "${PKGS[@]}"; do
     echo "INSTALLING: ${PKG}"
-    flatpak install "$PKG" --noconfirm --needed
+    flatpak install "$PKG"
 done
 echo -e "\nDone!\n"
 exit
