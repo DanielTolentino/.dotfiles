3600996b948f150b9c8ed60e54015f132fe7d71d
DanielTolentino <danieltolentino@protonmail.com>
2021-10-28 18:07:16 -0300

    add platpak installs

--- fish/config.fish
+++ fish/config.fish
@@ -83,6 +83,37 @@ function copy
         command cp $argv
     end
 end
+
+# Function for printing a column (splits input on whitespace)
+# ex: echo 1 2 3 | coln 3
+# output: 3
+function coln
+    while read -l input
+        echo $input | awk '{print $'$argv[1]'}'
+    end
+end
+
+# Function for printing a row
+# ex: seq 3 | rown 3
+# output: 3
+function rown --argument index
+    sed -n "$index p"
+end
+
+# Function for ignoring the first 'n' lines
+# ex: seq 10 | skip 5
+# results: prints everything but the first 5 lines
+function skip --argument n
+    tail +(math 1 + $n)
+end
+
+# Function for taking the first 'n' lines
+# ex: seq 10 | take 5
+# results: prints only the first 5 lines
+function take --argument number
+    head -$number
+end
+
 ### END OF FUNCTIONS ###
 
 ### ALIASES ###
