8766f396ba616b32c0adc9222c1a0deff24ed03e
Daniel Tolentino <danieltolentino@protonmail.com>
2022-06-14 13:03:44 -0300

    enable network and priting services

--- setup/post.sh
+++ setup/post.sh
@@ -1,10 +1,11 @@
 echo -e "\nFINAL SETUP AND CONFIGURATION"
 echo -e "\nEnabling essential services"
 
-#systemctl enable cups.service
-#sudo systemctl enable NetworkManager.service
+systemctl enable cups.service
+sudo systemctl enable NetworkManager.service
 sudo systemctl enable --now zramd
-sudo systemctl enable auto-cpufreq
-sudo systemctl start auto-cpufreq
+sudo systemctl enable --now systemd-resolved.service
+#sudo systemctl enable auto-cpufreq
+#sudo systemctl start auto-cpufreq
 echo "Finished"
 exit
